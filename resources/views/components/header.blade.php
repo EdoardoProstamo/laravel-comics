4da20cb6a09fdd9dd1cb5d70a5ffdc10f62cbbf0
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-08-06 00:50:33 +0200

    feat: Inizio progetto laravel-comics

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="container">
+        <img src="" alt="LOGO DC">
+        <nav>
+            <a href="#">CHARACTERS</a>
+            <a href="#">COMICS</a>
+            <a href="#">MOVIES</a>
+            <a href="#">TV</a>
+            <a href="#">GAMES</a>
+            <a href="#">COLLECTIBLES</a>
+            <a href="#">VIDEO</a>
+            <a href="#">FANS</a>
+            <a href="#">NEWS</a>
+            <a href="#">SHOP</a>
+        </nav>
+    </div>
+</header>
